3b76dbe2378a289c0abc3de5ccc56dbded5989a9
lenovo <lenovo@LAPTOP-0H44TNST>
2020-04-22 09:15:05 +0800

    添加项目文件。

---
+++ UI/Controllers/DrugController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class DrugController : Controller
+    {
+        // GET: Drug
+        //显示药品表
+        public ActionResult DrugShow()
+        {
+            return View();
+        }
+        //添加药品
+        public ActionResult DrugAdd()
+        {
+            return View();
+        }
+        //绑定药品分类
+        public ActionResult DrugTypeBandsel()
+        {
+            return View();
+        }
+        //添加药品分类
+        public ActionResult DrugTypeAdd()
+        {
+            return View();
+        }
+    }
+}
